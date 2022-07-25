f43db4062f3dff67a0cdba78549a825b0e0a7aa4
SmashedFrenzy16 <68993968+SmashedFrenzy16@users.noreply.github.com>
2022-07-25 19:21:41 +0500

    Update read-emails-imap.py

--- read-emails-imap.py
+++ read-emails-imap.py
@@ -1,2 +1,20 @@
 import imaplib
 import pprint
+
+host = input("Enter IMAP host: ")
+username = input("Enter username: ")
+password = input("Enter password: ")
+
+imap = imaplib.IMAP4_SSL(host)
+
+imap.login(username, password)
+
+imap.select('Inbox')
+
+tmp, data = imap.search(None, 'ALL')
+for num in data[0].split():
+	tmp, data = imap.fetch(num, '(RFC822)')
+	print('Message: {0}\n'.format(num))
+	pprint.pprint(data[0][1])
+	break
+imap.close()
