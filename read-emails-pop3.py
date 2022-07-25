fc44a60ba04305384cf5aa1e91e56f501041368a
SmashedFrenzy16 <68993968+SmashedFrenzy16@users.noreply.github.com>
2022-07-25 19:00:46 +0500

    Update read-emails-pop3.py

--- read-emails-pop3.py
+++ read-emails-pop3.py
@@ -1,1 +1,14 @@
 import poplib
+
+username = input("Enter username: ")
+password = input("Enter password: ")
+server = input("Enter POP3 server: ")
+port = input("Enter port: ")
+
+Mailbox = poplib.POP3_SSL(server, port)
+
+Mailbox.user(username)
+
+Mailbox.pass_(password)
+
+messages = len(Mailbox.list()[1])
