189ebdcac2a4268dfaf0ed85997db6cddee2406d
SmashedFrenzy16 <68993968+SmashedFrenzy16@users.noreply.github.com>
2022-07-25 19:04:51 +0500

    Update read-emails-pop3.py

--- read-emails-pop3.py
+++ read-emails-pop3.py
@@ -12,3 +12,12 @@ Mailbox.user(username)
 Mailbox.pass_(password)
 
 messages = len(Mailbox.list()[1])
+
+for i in range(messages):
+  
+  for msg in Mailbox.retr(i+1)[1]:
+    
+        print(msg)
+
+      
+Mailbox.quit()
